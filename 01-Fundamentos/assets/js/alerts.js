9f713b06a786dd6572cb40ca954b17a797b6f9e0
Hector <pugagarciah@gmail.com>
2023-09-25 22:57:27 -0600

    Fin de sección 2

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,7 @@
+// alert("Hola mundo");
+// let nombre=prompt("¿Cual es tu nombre?","Sin nombre");
+
+// console.log(nombre);
+// console.log('***'+nombre+'****');
+// const seleccion=confirm("¡Esta seguro de borrar esto?");
+// console.log(seleccion);
